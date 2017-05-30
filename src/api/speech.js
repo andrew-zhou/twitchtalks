307750f0e9147d1bb8afa0493323a03ea6952e07
jasonf7 <jason.z.fang@gmail.com>
2017-05-30 16:48:45 -0400

    fix up api

--- src/api/speech.js
+++ src/api/speech.js
@@ -4,8 +4,8 @@ const s3client = require('../lib/s3_client');
 const upload = (req, res) => {
 	// Upload to S3
 	const {file} = req;
-	const {originalname} = file;
-	const {uid,title} = req.headers;
+	const {filename, originalname} = file;
+	const {uid, title} = req.headers;
 
 	if (!uid) {
 		return res.status(403).json({error: 'No user provided.'});
@@ -15,14 +15,19 @@ const upload = (req, res) => {
 		return res.status(400).json({error: 'No speech file found.'});
 	}
 
-	s3client.upload(originalname, file, () => {
+	const fileNameByPeriod = originalname.split(".");
+	const fileExtension = fileNameByPeriod[fileNameByPeriod.length - 1];
+	const s3name = `${filename}.${fileExtension}`;
+
+	s3client.upload(s3name, file, () => {
 		// Create in firebase
-		const url = s3client.getUrl(originalname);
+		const url = s3client.getUrl(s3name);
 		const db = firebase.database();
 		const key = db.ref().child('speeches').push().key;
+
 		db.ref(`speeches/${key}`).set({
 			name: title,
-			uid: req.headers.uid,
+			uid,
 			url,
 		});
 		res.status(200).json({url});
--- src/lib/s3_client.js
+++ src/lib/s3_client.js
@@ -2,13 +2,15 @@ const s3 = require('s3');
 
 const bucket = 'jabber-speeches';
 
+const options = {
+	accessKeyId: 'AKIAJACKKFVYMO6WTNHQ',
+	secretAccessKey: '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',
+	region: 'us-east-2',
+	signatureVersion: 'v3', // Stuff breaks if we use v2 or v4
+}
+
 const client = s3.createClient({
-	s3Options: {
-		accessKeyId: 'AKIAJACKKFVYMO6WTNHQ',
-    secretAccessKey: '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',
-    region: 'us-east-2',
-		signatureVersion: 'v3',
-	},
+	s3Options: options,
 });
 
 const upload = (key, file, onSuccess, onError) => {
@@ -28,7 +30,7 @@ const upload = (key, file, onSuccess, onError) => {
 	uploader.on('end', onSuccess);
 };
 
-const getUrl = key => s3.getPublicUrlHttp(bucket, key);
+const getUrl = key => s3.getPublicUrl(bucket, key, options.region);
 
 module.exports = {
 	client,
